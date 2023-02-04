a2311a9964b8e227d98fcde1648177ebebdb3c1c
raviranjan <raviranjancp@gmail.com>
2023-02-04 16:56:40 +0530

    bugs fixed

--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ import { applyMiddleware, compose, createStore } from 'redux';
 import combineReducers from './redux/reducers/index.js';
 import thunk from 'redux-thunk';
 
-const store = createStore(combineReducers, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(combineReducers, compose(applyMiddleware(thunk)));
 
 export default store;
 
